d56def4ae1694bceaaa01a984f6a8a3b45421bae
lewibt01 <21316163+lewibt01@users.noreply.github.com>
2023-01-10 21:51:42 -0500

    Update swarmMove.lua

--- ComputerPrograms/Swarm/swarmMove.lua
+++ ComputerPrograms/Swarm/swarmMove.lua
@@ -5,15 +5,15 @@ ids = {3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18}
 
 rednet.open("top")
 
-function sendCommand(command)
-	rednet.send(ids[i],command,protocol)
+function sendCommand(id,command)
+	rednet.send(id,command,protocol)
 
 	local id,msg = rednet.receive(protocol,5)
 	return id,msg
 end
 
 for i=1,#ids do
-	local id,result = sendCommand("forward")
+	local id,result = sendCommand(ids[i],"forward")
 	print(id..":"..result)
 end
 
