e181313fd0df93bb1fa32c697a46ad2134fb5406
lewibt01 <21316163+lewibt01@users.noreply.github.com>
2023-01-10 23:48:22 -0500

    Update swarmMove.lua

--- ComputerPrograms/Swarm/swarmMove.lua
+++ ComputerPrograms/Swarm/swarmMove.lua
@@ -15,7 +15,7 @@ end
 for i=1,#ids do
 	local id,result = sendCommand(ids[i],"forward")
 	if(result) then 
-		print(id[i]..":"..result)
+		print(ids[i]..":"..result)
 	else
 		print("Failed to get response from "..ids[i])
 	end
