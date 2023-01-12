361265c7fd55480faaca67397049d32a4face27e
lewibt01 <21316163+lewibt01@users.noreply.github.com>
2023-01-12 10:31:28 -0500

    Create swarmBroadcastTest.lua

---
+++ TestScripts/swarmBroadcastTest.lua
@@ -0,0 +1,9 @@
+package.path = package.path..";C:\\GitRepositories\\ComputerCraftScripts\\APIs\\?.lua"
+package.path = package.path..";C:\\GitRepositories\\ComputerCraftScripts\\APIs\\Swarm\\?.lua"
+package.path = package.path..";C:\\GitRepositories\\ComputerCraftScripts\\Mocks\\?.lua"
+package.path = package.path..";C:\\GitRepositories\\ComputerCraftScripts\\ComputerPrograms\\Swarm\\?.lua"
+
+rednet = require("rednet")
+b = require("broadcastTest")
+
+b.broadcastTest("back")
