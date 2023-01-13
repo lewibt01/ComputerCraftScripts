07384872cbb158b78e65ca84ae491474a6309c44
lewibt01 <21316163+lewibt01@users.noreply.github.com>
2023-01-13 00:42:22 -0500

    adding basic functionality to swarm quarry host
    Hopefully this system is robust enough to perform on a server. Testing will tell.

--- ComputerPrograms/Swarm/swarmQuarryHost.lua
+++ ComputerPrograms/Swarm/swarmQuarryHost.lua
@@ -10,15 +10,20 @@ local function debug(...)
 	end
 end
 
+--[[Setup]]
 local protocol = "swarm:chunkMiner"
 local ids = {3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18}
 local locations = {}
 local orientations = {}
+local fuelLevels = {}
+local inventories = {}
 
---init dictionaries
+--init location data
 for i=1,#ids do
 	locations[ids[i]] = {0,0,0}
 	orientations[ids[i]] = 0
+	fuelLevels[ids[i]] = 0
+	inventories[ids[i]] = {}
 end
 
 --set protocol
@@ -27,5 +32,114 @@ s.protocol = protocol
 --open rednet
 rednet.open("top")
 
+--[[Functions]]
+function run(id,command)
+	return s.sendCommand(id,command,s.protocol)
+end
+
+function locate()
+	for i=1,#ids do
+		local target = ids[i]
+		locations[target] = run(target,"locate")
+	end
+end
+
+function orient()
+	for i=1,#ids do
+		local target = ids[i]
+		orientations[target] = run(target,"orient")
+	end
+end
+
+function forward()
+	s.distributeCommand(ids,"forward")
+end
+
+function back()
+	s.distributeCommand(ids,"back")
+end
+
+function up()
+	s.distributeCommand(ids,"up")
+end
+
+function down()
+	s.distributeCommand(ids,"down")
+end
+
+function turnLeft()
+	s.distributeCommand(ids,"turnLeft")
+end
+
+function turnRight()
+	s.distributeCommand(ids,"turnRight")
+end
+
+function dig()
+	s.distributeCommand(ids,"dig")
+end
+
+function digUp()
+	s.distributeCommand(ids,"digUp")
+end
+
+function digDown()
+	s.distributeCommand(ids,"digDown")
+end
+
+function place()
+	s.distributeCommand(ids,"place")
+end
+
+function placeUp()
+	s.distributeCommand(ids,"placeUp")
+end
+
+function placeDown()
+	s.distributeCommand(ids,"placeDown")
+end
+
+function drop()
+	s.distributeCommand(ids,"drop")
+end
+
+function dropDwon()
+	s.distributeCommand(ids,"dropDown")
+end
+
+function dropUp()
+	s.distributeCommand(ids,"dropUp")
+end
+
+function select(n)
+	local tmp = n % 17 --not the most accurate but close enough
+	s.distributeCommand(ids,"select "..tmp)
+end
+
+function getFuelLevel()
+	for i=1,#ids do
+		local target = ids[i]
+		fuelLevels[target] = run(target,"getFuelLevel")
+	end
+end
+
+--[[Main Execution]]
+--init
+locate()
+orient()
+
+--get turtles into position
+digDown()
+down()
+digDown()
+down()
+
+--DEBUG: attampt to reverse other actions
+up()
+placeDown()
+up()
+placeDown()
+
+
 --run the commands
-s.distributeCommand(ids,"forward")
+-- s.distributeCommand(ids,"forward")
