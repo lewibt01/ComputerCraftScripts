1faa3e650ca3e5f0d42c7726dfa75394d72e454f
lewibt01 <21316163+lewibt01@users.noreply.github.com>
2023-01-18 00:30:00 -0500

    adding neural interface scripts and alternate quarry program

---
+++ ComputerPrograms/Swarm/swarmMiningWellsHost.lua
@@ -0,0 +1,190 @@
+package.path = package.path..";/api/?.lua"
+
+s = require("swarm")
+t = require("tableUtils")
+n = require("numberUtils")
+
+local debugFlag = false
+local function debug(...)
+	if(debugFlag) then
+		print(...)
+	end
+end
+
+--[[Setup]]
+local protocol = "swarm:chunkMiner"
+local ids = {3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18}
+local locations = {}
+local orientations = {}
+local fuelLevels = {}
+local inventories = {}
+
+--init location data
+for i=1,#ids do
+	local t = ids[i]
+	locations[t] = {0,70,0} --undo this hardcoding later once gps decides to work
+	orientations[t] = 0
+	fuelLevels[t] = 0
+	inventories[t] = {}
+end
+
+--set protocol
+s.protocol = protocol
+
+--open rednet
+peripheral.find("modem",rednet.open)
+
+--[[Functions]]
+function run(id,command)
+	return s.sendCommand(id,command,s.protocol)
+end
+
+function locate()
+	for i=1,#ids do
+		local target = ids[i]
+		locations[target] = run(target,"locate")
+	end
+end
+
+function orient()
+	for i=1,#ids do
+		local target = ids[i]
+		orientations[target] = run(target,"orient")
+	end
+end
+
+function forward()
+	s.distributeCommand(ids,"forward")
+end
+
+function back()
+	s.distributeCommand(ids,"back")
+end
+
+function up()
+	s.distributeCommand(ids,"up")
+end
+
+function down()
+	s.distributeCommand(ids,"down")
+end
+
+function turnLeft()
+	s.distributeCommand(ids,"turnLeft")
+end
+
+function turnRight()
+	s.distributeCommand(ids,"turnRight")
+end
+
+function dig()
+	s.distributeCommand(ids,"dig")
+end
+
+function digUp()
+	s.distributeCommand(ids,"digUp")
+end
+
+function digDown()
+	s.distributeCommand(ids,"digDown")
+end
+
+function place()
+	s.distributeCommand(ids,"place")
+end
+
+function placeUp()
+	s.distributeCommand(ids,"placeUp")
+end
+
+function placeDown()
+	s.distributeCommand(ids,"placeDown")
+end
+
+function drop()
+	s.distributeCommand(ids,"drop")
+end
+
+function dropDwon()
+	s.distributeCommand(ids,"dropDown")
+end
+
+function dropUp()
+	s.distributeCommand(ids,"dropUp")
+end
+
+function attack()
+	s.distributeCommand(ids,"attack")
+end
+
+function attackUp()
+	s.distributeCommand(ids,"attackUp")
+end
+
+function attackDown()
+	s.distributeCommand(ids,"attackDown")
+end
+
+function refuel()
+	s.distributeCommand(ids,"refuel")
+end
+
+function select(n)
+	local tmp = n % 17 --not the most accurate but close enough
+	s.distributeCommand(ids,"select "..tmp)
+end
+
+function getFuelLevel()
+	for i=1,#ids do
+		local target = ids[i]
+		fuelLevels[target] = run(target,"getFuelLevel")
+	end
+end
+
+--[[Aggregate functions]]
+function refuelStep()
+	--clear the block above the turtle
+	digUp()
+
+	--place the dump chest to empty a slot for fuel
+	select(16)
+	placeUp()
+
+	--dump the first slot
+	select(1)
+	dropUp()
+
+	--recollect the dump chest
+	select(16)
+	digUp()
+
+	--place down the fuel chest and retrieve some fuel
+	select(15)
+	placeUp()
+	select(1)
+	suckUp()
+
+	for i=1,4 do
+		refuel()
+	end
+
+	--retrieve the fuel chest
+	select(15)
+	digUp()
+end
+
+function dumpInventory()
+	select(16) --reserved slot for dump chest
+	digUp()
+	placeUp() --place dump chest above
+
+	for i=1,14 do
+		select(i)
+		dropUp()
+	end
+
+	select(16)
+	digUp()
+	select(1)
+end
+
---
+++ PlethoraMod/NeuralInterface/jump.lua
@@ -0,0 +1,40 @@
+--program to jump high when 'g' is pressed
+local neural = peripheral.find("neuralInterface")
+if not neural then error("Must have a neural interface",0) end
+
+local meta = {}
+local hover = false
+
+function jumpLoop()
+	while(true) do
+		local event,key = os.pullEvent()
+		if(event == "key" and key == keys.g) then
+			neural.launch(0,-90,2)
+		end
+	end
+end
+
+function metadataLoop()
+	while(true) do
+		meta = neural.getMetaOwner()
+	end
+end
+
+function safeFallLoop()
+	while(true) do
+		local blocks = neural.scan()
+		for y=0,-8,-1 do
+			--scan the block below us
+			local block = blocks[1 + (8 + (8 + y)*17 + 8*17^2)]
+			if(block.name == "minecraft:air") then 
+				if(meta.motionY < -0.3) then
+					modules.launch(0,-90,math.min(4,meta.motionY / -0.5))
+				end
+				break
+			end
+		end
+	end
+end
+
+--main loops
+parallel.waitForAny(jumpLoop,metadataLoop,safeFallLoop)
