e06268fa71231fff9ca32b2ae413a2f9e239a297
lewibt01 <21316163+lewibt01@users.noreply.github.com>
2025-11-13 22:54:04 -0500

    Update reactorStatus.lua

--- ComputerPrograms/DraconicEvolution/reactorStatus.lua
+++ ComputerPrograms/DraconicEvolution/reactorStatus.lua
@@ -3,6 +3,11 @@ local reactorName = "draconic_reactor_0"
 local inGateName = "flow_gate_0"
 local outGateName = "flow_gate_1"
 
+function round(n, digits)
+    local mult = 10^(digits or 0)
+    return math.floor(n * mult + 0.5) / mult
+end
+
 function getWiredModem()
     -- Find the wired modem
     local modem = peripheral.find("modem")
@@ -37,22 +42,41 @@ end
 --[[Monitor Printing]]
 function printHorizontalBar(monitor,x,y,name,max,current)
     local width,height = monitor.getSize()
-    monitor.setCursorPos(x,y)
-    monitor.write(name)
-    monitor.setCursorPos(x,y+1)
-
     -- get percentage...
     local percent = current / max
     local charWidth = width
     local numChars = math.ceil(percent * charWidth)
+
+    monitor.setCursorPos(x,y)
+    monitor.write(name.." ("..round(percent,2).."%)")
+    monitor.setCursorPos(x,y+1)
+
+    
     monitor.write(string.rep("|",numChars))
 end
 
 function draw(monitor,reactor,inGate,outGate)
     local info = reactor.getReactorInfo()
+
+    --[[
+        energySaturation,
+        failSafe,
+        fieldDrainRate,
+        fieldStrength,
+        fuelConversion,
+        fuelConversionRate,
+        generationRate,
+        maxEnergySaturation,
+        maxFieldStrength,
+        maxFuelConversion,
+        status,
+        temperature
+    ]]
     printHorizontalBar(monitor,1,1,"Energy Saturation",info.maxEnergySaturation,info.energySaturation)
     printHorizontalBar(monitor,1,3,"Field Strength",info.maxFieldStrength,info.fieldStrength)
     printHorizontalBar(monitor,1,5,"Fuel Conversion",info.maxFuelConversion,info.fuelConversion)
+    printHorizontalBar(monitor,1,7,"Temperature",10000,info.temperature)
+
 end
 
 --[[Reactor Handling]]
@@ -69,7 +93,7 @@ local outGate = peripheral.wrap(outGateName)
 local inGate = peripheral.wrap(inGateName)
 
 -- Configure the monitor
-mon.setTextScale(1.5)
+mon.setTextScale(1.3)
 mon.setBackgroundColor(colors.black)
 mon.setTextColor(colors.white)
 mon.clear()
